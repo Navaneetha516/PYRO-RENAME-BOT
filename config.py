5674554842d172ba80c405d8156494c217232e04
Navaneetha516 <121594658+Navaneetha516@users.noreply.github.com>
2023-12-02 11:28:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/52807f093e8b1f225a37a.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1439022100').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "-1001688675040") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "1001688675040"))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
